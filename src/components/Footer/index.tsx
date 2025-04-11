9949c5bf246b3f34fba8e8850c68abf5de0a4043
tomii-dev-fullstack <tomiiruglio98@gmail.com>
2025-04-11 15:20:24 -0300

    first commit

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -3,7 +3,6 @@ import { withTranslation, TFunction } from "react-i18next";
 import { SvgIcon } from "../../common/SvgIcon";
 import Container from "../../common/Container";
 
-import i18n from "i18next";
 import {
   FooterSection,
   Title,
@@ -13,12 +12,8 @@ import {
   Para,
   Large,
   Chat,
-  Empty,
   FooterContainer,
   Language,
-  Label,
-  LanguageSwitch,
-  LanguageSwitchContainer,
 } from "./styles";
 
 interface SocialLinkProps {
@@ -27,10 +22,6 @@ interface SocialLinkProps {
 }
 
 const Footer = ({ t }: { t: TFunction }) => {
-  const handleChange = (language: string) => {
-    i18n.changeLanguage(language);
-  };
-
   const SocialLink = ({ href, src }: SocialLinkProps) => {
     return (
       <a
